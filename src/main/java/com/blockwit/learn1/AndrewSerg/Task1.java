76013056a04eb3d538d808fde3ecaee71cbaadc8
AndrewSerg <sergsergsergeev95@gmail.com>
2022-01-09 17:18:13 +0300

    finish

--- src/main/java/com/blockwit/learn1/AndrewSerg/Task1.java
+++ src/main/java/com/blockwit/learn1/AndrewSerg/Task1.java
@@ -2,6 +2,21 @@ package com.blockwit.learn1.AndrewSerg;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("fgfnjm");
+        String[] names = {"Alisa", "Bob", "john", "Karl", "Alex"};
+        String[] alfabit = {"a", "b", "c", "d"};
+        int[] numbers = {12, 24, 29, 45, 55};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+            for (int j = 0; j < alfabit.length; j++) {
+                System.out.println(alfabit[j]);
+            }
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] > 30) {
+                    System.out.println(numbers[j]);
+                }
+            }
+        }
     }
 }
+
