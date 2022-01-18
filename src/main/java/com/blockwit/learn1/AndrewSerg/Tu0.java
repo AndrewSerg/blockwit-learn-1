c7af9865fa68702b91bad39b7218915bd1a2fa74
AndrewSerg <sergsergsergeev95@gmail.com>
2022-01-18 19:02:12 +0300

    урок 7 задача 1

---
+++ src/main/java/com/blockwit/learn1/AndrewSerg/Tu0.java
@@ -0,0 +1,21 @@
+package com.blockwit.learn1.AndrewSerg;
+
+public class Tu0 {
+    public static void main(String[] args) {
+        int[] number = {12, 7, 89, -4, 8, 9};
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+        int temp=0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                if (number[i]>number[j]){
+                    temp=number[i];
+                    number[i]=number[j];
+                    number[j]=temp;
+                }
+            }
+            System.out.println(number[i]);
+        }
+    }
+}
